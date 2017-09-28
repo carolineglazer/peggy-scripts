b35d82713ec911049f9a5b77fc37b5282d6f56df
Caroline Glazer <carolineroseglazer@gmail.com>
2017-09-28 10:31:07 -0500

    add gen_am

---
+++ edit_distance_amer.py
@@ -0,0 +1,27 @@
+from music21 import *
+import os
+import operator
+
+all_diffs = {}
+
+for f in sorted(os.listdir('../gen_am/')):
+	if f.endswith('.xml'):
+		print('###### '+f+' ######')
+		base_peggy = ('../gen_am/'+f)
+		diffs = []
+		total = 0
+		for i in sorted(os.listdir('../gen_am/')):
+			if i.endswith('.xml'):
+				peggy_pair = omr.evaluators.OmrGroundTruthPair(omr=converter.parse(base_peggy),ground=converter.parse('../gen_am/'+i))
+				diff = peggy_pair.getDifferences()
+				diffs.append(diff)
+				total += diff
+				print(i+': '+str(diff))
+		all_diffs[f] = total
+		print(' ')
+
+# print(all_diffs)
+sorted_all_diffs = sorted(all_diffs.items(), key=operator.itemgetter(1))
+print(sorted_all_diffs)
+print('winner: '+str(sorted_all_diffs[0]))
+print('average edit distance for winner: '+str(sorted_all_diffs[0][1]/len(all_diffs)))
